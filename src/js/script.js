23063502e6d02a2efcbff377eb29d9c229af2a59
Nae Cosmin <cosminnae00@yahoo.com>
2022-04-25 14:40:32 +0300

    API Functional

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,5 @@
+// Phone Menu
+
 const phoneMenu = document.querySelector('.nav-links-login');
 
 const btn = document
@@ -6,7 +8,36 @@ const btn = document
     phoneMenu.classList.toggle('active');
   });
 
-const shortURL = async () => {
-  const response = await fetch('https://api.shrtco.de/v2/');
-  const data = await response.json();
-};
+// Input
+
+const urlsRow = document.querySelector('.urls');
+const input = document.querySelector('input');
+const err = document.querySelector('.err');
+
+const inputBtn = document
+  .querySelector('.text-btn')
+  .addEventListener('click', () => {
+    const inputText = document.querySelector('input').value;
+
+    if (inputText === '') {
+      input.style.border = '1px solid red';
+      err.style.display = 'block';
+    } else {
+      input.style.border = 'none';
+      err.style.display = 'none';
+    }
+
+    const shortURL = async () => {
+      const response = await fetch(
+        `https://api.shrtco.de/v2/shorten?url=${inputText}`
+      );
+      const data = await response.json();
+      const url = data.result.short_link3;
+
+      urlsRow.innerHTML += ` <div> <p> ${inputText} </p>   <p> ${url} </p> 
+      
+      <button> Copy Link <button>
+      </div>  `;
+    };
+    shortURL();
+  });
